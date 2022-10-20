268ad892e7687e4a95ce36133b698ccba59cf4bc
aytacipekel <aytacipekel@gmail.com>
2022-10-20 19:43:00 +0300

    collection finish

--- resources/views/favorite_collection.blade.php
+++ resources/views/favorite_collection.blade.php
@@ -28,11 +28,40 @@
                                 </a>
                             </td>
                             <td>
-                                <li class="dropdown-item"><a class="dropdown-link tx-13 tx-gray-500" href="{{ route('collection_delete' , $collection->id) }}"><i class="icon-close mr-2"></i>Sil</a></li>
+
+
+
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                    Sil
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Koleksiyon Sil!</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <li class="dropdown-item">Bu Koleksiyonu silmek istediğinize emin misiniz?</li>
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <a class="dropdown-link tx-13 tx-gray-500" href="{{ route('collection_delete' , $collection->id) }}">
+                                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Sil</button></a>
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
             </table>
+
         </div>
     </div>
 @endsection
